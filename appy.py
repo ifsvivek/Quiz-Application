560182584aa12ca537e897bfa32caf9af4c33d70
momishbl <monishbl159@gmail.com>
2024-06-30 21:59:55 +0530

    chore: Update CSS margin and remove unused test.py file

--- appy.py
+++ appy.py
@@ -1,19 +1,18 @@
-from flask import *
+from flask import Flask, request, redirect, url_for, render_template
 import mysql.connector
 import random
 import openpyxl
 
 app = Flask(__name__)
 
-
+qu_count = 0
+count = 0
 allowed_urls = ["/"]
 questions = []
 score = 0
 username = ""
 ADMIN_USERNAME = "admin"
 error_message = ""
-
-
 
 wb = openpyxl.load_workbook("interview_questions_mcq.xlsx")
 ws = wb.active
@@ -35,7 +34,6 @@ def get_db_connection():
         host="localhost", user="root", password="9249", database="example"
     )
 
-
 # Database query execution
 def query_db(query, args=(), one=False):
     conn = get_db_connection()
@@ -47,12 +45,10 @@ def query_db(query, args=(), one=False):
     conn.close()
     return (rv[0] if rv else None) if one else rv
 
-
 # Fetch total number of questions
 def get_total_questions():
     result = query_db("SELECT COUNT(*) as total FROM qa", one=True)
     return result["total"]
-
 
 def select_random_question():
     total_questions = get_total_questions()
@@ -65,7 +61,6 @@ def select_random_question():
     ran = random.choice(available_questions)
     return ran
 
-
 def check_answer(question, answer):
     if answer is None:
         return False
@@ -77,7 +72,6 @@ def check_answer(question, answer):
 
     data = query_db("SELECT answer FROM qa WHERE qno=%s;", (question,), one=True)
     return data and data["answer"] == answer_int
-
 
 @app.route("/", methods=["GET", "POST"])
 def home():
@@ -95,7 +89,6 @@ def home():
         score = 0
         return render_template("index.html", error_message=error_message)
 
-
 @app.route("/user_login", methods=["POST", "GET"])
 def user_login():
     global username, allowed_urls
@@ -106,7 +99,6 @@ def user_login():
         return redirect(url_for("quiz"))
     return render_template("index.html")
 
-
 @app.route("/admin_authenticate", methods=["POST"])
 def admin_authenticate():
     allowed_urls.append("/admin_authenticate")
@@ -116,14 +108,15 @@ def admin_authenticate():
     allowed_urls.append("/admin")
     return redirect(url_for("admin"))
 
-
 @app.route("/quiz", methods=["GET", "POST"])
 def quiz():
-    global error_message
+    global error_message, count, qu_count
     if "/quiz" not in allowed_urls:
         error_message = "Access Denied - Please login to continue."
         return redirect(url_for("home"))
     else:
+        count += 1
+        qu_count += 1
         global score, questions, username
         question = request.form.get("qno")
         user_answer = request.form.get("answer")
@@ -142,16 +135,16 @@ def quiz():
                     "user_answer": user_answer,
                     "correct": correct,
                     "answer": query_db(
-                        "SELECT  choice%s  FROM qa WHERE qno=%s;",
+                        "SELECT choice%s FROM qa WHERE qno=%s;",
                         (
                             int(user_answer) + 1,
                             question,
                         ),
                         one=True,
-                    ),
+                    )["choice%s" % (int(user_answer) + 1)],
                     "question": query_db(
                         "SELECT question FROM qa WHERE qno=%s;", (question,), one=True
-                    ),
+                    )["question"],
                     "correct_choice": query_db(
                         "SELECT choice%s FROM qa WHERE qno=%s;",
                         (
@@ -166,23 +159,31 @@ def quiz():
                             question,
                         ),
                         one=True,
-                    ),
+                    )["choice%s" % (int(
+                                query_db(
+                                    "SELECT answer FROM qa WHERE qno=%s;",
+                                    (question,),
+                                    one=True,
+                                )["answer"]
+                            )
+                            + 1)],
                 }
             )
         total_questions = get_total_questions()
-        if len(questions) >= total_questions:
+
+        if len(questions) >= total_questions or count-1 >= 10:
             allowed_urls.append("/result")
             return redirect(url_for("result"))
         ran = select_random_question()
         data = query_db("SELECT * FROM qa WHERE qno=%s;", (ran,), one=True)
         return render_template(
             "quiz.html",
+            qu_count=qu_count,
             data=data,
             count=count,
             total_questions=total_questions,
             username=username,
         )
-
 
 @app.route("/result", methods=["GET", "POST"])
 def result():
@@ -198,7 +199,6 @@ def result():
             questions=questions,
             username=username,
         )
-
 
 @app.route("/admin", methods=["GET", "POST"])
 def admin():
@@ -217,11 +217,16 @@ def admin():
             choice3 = request.form.get("choice3")
             choice4 = request.form.get("choice4")
 
+
             if action == "add":
                 query_db(
                     "INSERT INTO qa (qno, question, answer, choice1, choice2, choice3, choice4) VALUES (%s, %s, %s, %s, %s, %s, %s);",
                     (qno, question, answer, choice1, choice2, choice3, choice4),
                 )
+                wb = openpyxl.load_workbook("interview_questions_mcq.xlsx")
+                ws = wb.active
+                ws.append([question, "\n".join([choice1, choice2, choice3, choice4]), answer, "easy"])
+                wb.save("interview_questions_mcq.xlsx")
                 return "Question Added Successfully"
             elif action == "remove":
                 query_db("DELETE FROM qa WHERE qno=%s;", (qno,))
@@ -231,12 +236,21 @@ def admin():
                     "UPDATE qa SET question=%s, answer=%s, choice1=%s, choice2=%s, choice3=%s, choice4=%s WHERE qno=%s;",
                     (question, answer, choice1, choice2, choice3, choice4, qno),
                 )
+                #update in excel sheet
+                wb = openpyxl.load_workbook("interview_questions_mcq.xlsx")
+                ws = wb.active
+                for row in ws.iter_rows(min_row=2, values_only=True):
+                    if row[0] == question:
+                        ws.delete_rows(row[0].row)
+                        break
+                ws.append([question, "\n".join([choice1, choice2, choice3, choice4]), answer, "easy"])
+                wb.save("interview_questions_mcq.xlsx")
+
                 return "Question Modified Successfully"
             else:
                 return "Invalid Action", 400
 
         return render_template("admin.html")
-
 
 @app.route("/admin/question/<qno>", methods=["GET"])
 def get_question(qno):
@@ -245,16 +259,13 @@ def get_question(qno):
         return question
     return {}, 404
 
-
-
 if __name__ == "__main__":
-    mydb=get_db_connection()
-    cursor=mydb.cursor()
+    mydb = get_db_connection()
+    cursor = mydb.cursor()
     cursor.execute("DROP TABLE IF EXISTS qa")
-    cursor.execute("CREATE TABLE IF NOT EXISTS qa (qno INT PRIMARY KEY, question VARCHAR(255), answer INT, difficulty varchar(255), choice1 VARCHAR(255), choice2 VARCHAR(255), choice3 VARCHAR(255), choice4 VARCHAR(255))")
-    insert_query="INSERT INTO qa (question, answer, difficulty, choice1, choice2, choice3, choice4) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    cursor.execute("CREATE TABLE qa (qno INT PRIMARY KEY AUTO_INCREMENT, question VARCHAR(255), answer INT, difficulty varchar(255), choice1 VARCHAR(255), choice2 VARCHAR(255), choice3 VARCHAR(255), choice4 VARCHAR(255))")
+    insert_query = "INSERT INTO qa (question, answer, difficulty, choice1, choice2, choice3, choice4) VALUES (%s, %s, %s, %s, %s, %s, %s)"
     for question, details in interview_questions_mcq_dict.items():
-        # Preparing data tuple for insertion
         data_tuple = (
             question,
             details["answer_index"],
@@ -267,4 +278,3 @@ if __name__ == "__main__":
         cursor.execute(insert_query, data_tuple)
     mydb.commit()
     app.run(debug=True)
-
